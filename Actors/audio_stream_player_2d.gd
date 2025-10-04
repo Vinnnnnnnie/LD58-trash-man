cb2419b2d3a62709b0e61907596de4fa9c5c83b4
Vinnnnnnnie <vincentowens72@gmail.com>
2025-10-04 19:41:18 +0100

    Added trees

---
+++ Actors/audio_stream_player_2d.gd
@@ -0,0 +1,28 @@
+extends AudioStreamPlayer2D
+var screech = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_car_drift() -> void:
+	screech_tyre()
+	screech = false
+	pass # Replace with function body.
+
+func screech_tyre():
+	if screech == true:
+		pitch_scale = randf_range(0.8, 1.2)
+		play(0)
+		
+		
+
+func _on_car_not_drift() -> void:
+	stop()
+	screech = true
