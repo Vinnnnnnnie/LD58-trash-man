3cb630c5d323ebf55f32eb2792f83e245fa2143c
Vinnnnnnnie <vincentowens72@gmail.com>
2025-10-05 14:45:59 +0100

    Engine sounds fixed tweening bug, glasses upgrade, other upgrades implemented

--- GameUI/receiving_call.gd
+++ GameUI/receiving_call.gd
@@ -3,29 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	cool_rotation()
-
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func cool_rotation():
-	var tween = get_tree().create_tween()
-	var tween2 = get_tree().create_tween()
-	#tween.tween_property(self, 'rotation', -PI/28, 0.5)
-	#tween.tween_property(self, 'rotation', PI/28, 0.5)
-	#tween.tween_property(self, 'rotation', -PI/28, 0.5)
-	#tween.tween_property(self, 'rotation', PI/28, 0.5)
-
-	tween.tween_callback(cool_rotation_back)
-	
-func cool_rotation_back():
-	var tween = get_tree().create_tween()
-	var tween2 = get_tree().create_tween()
-
-	#tween.tween_property(self, 'rotation', -PI/28, 0.5)
-	#tween.tween_property(self, 'rotation', PI/28, 0.5)
-	#tween.tween_property(self, 'rotation', -PI/28, 0.5)
-	#tween.tween_property(self, 'rotation', PI/28, 0.5)
-	
-	tween.tween_callback(cool_rotation)
